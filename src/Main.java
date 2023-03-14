a6fca49c1e72d86676e169451513ced62f485c01
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-14 11:43:23 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // Användaren skall mata in en mening med minst två ord
+        // Skriv därefter ut:
+        //   - Antal tecken
+        //   - Första ordet
+        //   - Sista ordet
+
+
+    }
+}
